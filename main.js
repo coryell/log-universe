75b7ebefcdad6ebe12017356008d3ba0b8f59fd4
Cutter Coryell <14116109+coryell@users.noreply.github.com>
2026-02-07 21:40:08 -0800

    Decrease opacity of source link.

--- main.js
+++ main.js
@@ -900,7 +900,7 @@ d3.json('/data.json').then(data => {
 
         let sourceLink = "";
         if (d.sources && d.sources[dim]) {
-          sourceLink = ` <a href="${d.sources[dim]}" target="_blank" style="color: #00aaff; text-decoration: none; margin-left: 5px;">[source]</a>`;
+          sourceLink = ` <a href="${d.sources[dim]}" target="_blank" style="color: #00aaff; text-decoration: none; margin-left: 5px; opacity: 0.7;">[source]</a>`;
         }
 
         return `<div class="infobox-row"><span class="infobox-label">${label}:</span>${txt}${sourceLink}</div>`;
