7d86ebb4c1d3820c2c97313d09fed49bb5a75e27
Cutter Coryell <14116109+coryell@users.noreply.github.com>
2026-02-08 13:29:21 -0800

    Editor: move and rename files.

--- data_editor/server.js
+++ data_editor/server.js
@@ -8,14 +8,14 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
-const dataPath = path.join(__dirname, 'public', 'data.json');
+const dataPath = path.join(__dirname, '..', 'public', 'data.json');
 
 app.use(express.json({ limit: '50mb' }));
-app.use(express.static(path.join(__dirname), { index: false })); // Serve editor.html from root if desired, or simpler below
+app.use(express.static(path.join(__dirname), { index: false })); // Serve index.html from root if desired, or simpler below
 
 // Serve editor
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'editor.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Get Data
