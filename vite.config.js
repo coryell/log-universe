a06be0bde07dddf60fcd301a872f635952b1f60c
Cutter Coryell <14116109+coryell@users.noreply.github.com>
2026-02-17 21:26:03 -0800

    Add license rollup.

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import license from 'rollup-plugin-license';
+import { resolve } from 'path';
+
+export default {
+    build: {
+        rollupOptions: {
+            plugins: [
+                license({
+                    thirdParty: {
+                        output: {
+                            file: resolve('dist', 'THIRD-PARTY-LICENSES.txt'),
+                            encoding: 'utf-8',
+                        },
+                        allow: '(MIT OR ISC OR BSD-2-Clause OR BSD-3-Clause OR Apache-2.0 OR 0BSD)',
+                    },
+                }),
+            ],
+        },
+    },
+};
